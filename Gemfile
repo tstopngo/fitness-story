5fc95c3cf5f48915417bd53a116e547a45a172cf
Tracy Ngo <tngo1206@gmail.com>
2019-06-15 14:53:50 -0700

    update

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem 'tux'
+gem 'pry'
 
 group :test do
   gem 'rspec'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,52 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  # GET: /login
+  get "/login" do
+    if logged_in?
+      redirect "/users/:id"
+    else
+      erb :"/users/login.html"
+    end
+  end
+
+  # POST: /login
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "users/#{@user.username}"
+    else
+      redirect "/login"
+    end
   end
 
   # GET: /users/new
   get "/users/new" do
-    erb :"/users/new.html"
+    if logged_in?
+      redirect "/users/#{session[:user_id]}"
+    else
+      erb :"/users/new.html"
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  # POST: /users/new
+  post "/users/new" do
+    @user = User.find_by(:username => params[:username], :email => params[:user])
+    if @user
+      redirect '/login'
+    else
+      if params.values.each.include?("")
+        redirect '/signup'
+      else
+        @user = User.create(:email => params[:email], :username => params[:username], :password => params[:password])
+        session[:user_id] = @user.id
+        @user.autenticate(params[:password])
+        redirect "users/#{@user.username}"
+      end
+    end
   end
 
-  # GET: /users/5
+  # GET: /users/username
   get "/users/:id" do
     erb :"/users/show.html"
   end
@@ -30,8 +61,11 @@ class UsersController < ApplicationController
     redirect "/users/:id"
   end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  get '/logout' do
+    session.clear
+    redirect "/login"
   end
+
+
+
 end
